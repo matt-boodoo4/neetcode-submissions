5a0548f37f49c3ae70a3f6ced7dfcf459f7d6da5
Matthew Boodoo <38621634+matt-boodoo4@users.noreply.github.com>
2026-08-26 21:38:18 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freqS = {}
+        freqT = {}
+        # if both strings have different lengths, return false 
+        if len(s) != len(t):
+            return False
+        for char in s:
+            val = freqS.get(char)
+            if val:
+                val = val+1
+                freqS[char] = val
+            else:
+                freqS[char] = 1
+        for char in t:
+            val = freqT.get(char)
+            if val:
+                val+=1
+                freqT[char] = val
+            else:
+                freqT[char] = 1
+        
+        for key, val in freqS.items():
+            if freqT.get(key) != val:
+                return False
+        for key, val in freqT.items():
+            if freqS.get(key)!= val:
+                return False
+        return True
+        
+        
